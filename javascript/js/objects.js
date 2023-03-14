8777eefedf1e5ea8c566784f8cd1b37766a51662
Shkola Olga <olga_shkola@km-union.ru>
2023-03-14 20:52:46 +0300

    Add objects.js

--- javascript/js/objects.js
+++ javascript/js/objects.js
@@ -220,3 +220,136 @@ console.log(user2.name); // Петр
 
 user1.getFullName(); //
 user2.getFullName(); //
+
+// Objects.hasOwnProperty()
+// Создадим объект Tiger, прототипом которого будет Predator:
+const Predator = {
+    food: "meat",
+}
+function Tiger(name) {
+    this.name = name
+}
+
+Tiger.prototype = Predator; //определяем прототип
+
+const tiger = new Tiger("Vasya");
+
+tiger.hasOwnProperty("name"); // true
+tiger.hasOwnProperty("food"); // false
+
+"name" in tiger; //true
+"food" in tiger; //true
+// Метод Objects.hasOwnProperty() определяет, содержит ли объект
+// указанное свойство в качестве собственного свойства объекта.
+// В отличие от оператора in, он не проверяет наличие указанного
+// свойства по цепочке прототипов.
+
+// Метод Object.create(prototype) создает новый объект с указанным объектом прототипа (prototype):
+const tiger = Object.create(predator);
+tiger.food; //meat
+
+// Метод Object.assign(target, ...sources)
+// Создает новый объект путем копирования значений всех собственных перечисляемых свойств
+// из одного или более исходных объектов в целевой объект. В новом стандарте есть аналог - ...
+const person = {
+    name: "Frederic",
+}
+
+const account = {
+    balance: 14000,
+}
+
+const info1 = Object.assign({}, person, account);
+info1.name; // 'Frederic'
+info1.balance; // 14000
+
+// современный способ
+const info2 = { ...person };
+info2.name; // 'Frederic'
+let a = {a: 123, b: "sda", c: true};
+let b = {a: 42324, d: "dhbcueybufy"};
+{...a, ...b} // => {a: 42324, b: "sda", c: true, d: "dhbcueybufy"};
+
+// Еще способ (не копирует методы)
+let newObj = JSON.parse(JSON.stringify(person));
+JSON.stringify(a) // "{\"a\": 123, \"b\": \"sda\", \"c\": true}"
+JSON.parse({\"a\": 123, \"b\": \"sda\", \"c\": true}") // {a: 123, b: "sda", c: true}
+
+// Еще можно циклом for in
+
+// Глубокое клонирование (рекурсивное клонирование).
+// если свойства являются ссылками на другие объекты
+let car1 = {
+  name: "Ford",
+  engine: {
+    volume: 2.0,
+    eco: 5,
+  },
+}
+
+let car2 = Object.assign({}, user); // клон car1
+
+car1.engine === car2.engine;
+// true, один и тот же объект, а нам хотелось бы клон.
+// Для глубокого клонирования следует в цикле клонирования проверять,
+// не является ли значение car1[key] объектом, и если это так - скопировать
+// и его структуру тоже. Можно использовать готовую реализацию - рекурсивный метод
+// _.cloneDeep(obj) из JavaScript-библиотеки lodash.
+
+// Методы статических объектов (статические данные получают без создания объектов)
+// Object.keys(obj) | Object.values(obj)
+// - keys - возвращает массив, содержащий имена всех собственных перечисляемых свойств переданного объекта
+// - values - возвращает массив значений перечисляемых свойств объекта в том же порядке, что и цикл for...in
+Object.key(info); // ["name", "balance"]
+Object.values(info); // ["Frederic", 14000]
+
+// Метод Object.entries(obj) - массив пар ключ-значение
+// Метод возвращает массив собственых перечисляемых свойств указанного объекта в формате
+// [key, value] в том же порядке, что и в цикле for...in
+Object.entries(info); // [["name", "Frederic"], ["balance', 14000]]
+
+// Деструктуризация - механизм, позволяющий сохранить значения в определенном алгоритме
+for (const [key, value] of Object.entries(obj)) {
+    console.log(`${key} ${value}`);
+}
+
+Object.entries(obj).forEach(([key, value]) => {
+    console.log(`${key} ${values}`);
+});
+
+for (const key of Object.key(obj)) {
+    console.log(`${key} ${values}`);
+}
+let keys = Object.keys(obj);
+for (let i = 0; i < keys.length; i++) {
+    console.log(`${key[i]} ${values[i]}`);
+}
+
+// Работа с DATE
+let tiday = new Date(1995, 11, 17) // используется как конструктор
+let start = Date.now() // используется как объект
+new Date();
+// Интернационализировать даты - Intl:
+const date = new Date();
+
+const formatter = new Intl.DateTimeFormat("ru");
+gotmatter.format(date); // "23.11.2018"
+
+// Методы объекта Math
+Math.PI
+Math.E
+Math.random();
+Math.max
+Math.min
+Math.floor(2.6); // в меньшую сторону
+Math.round(2.6); // до ближайшего целого
+Math.ceil(2.1); // в большую сторону
+Math.trunc(3.15) // отсекает дробную часть - разница с Math.floor при работе с отрицательными числами
+Math.abs(-2); //2
+
+// Number.toFixed() - возвращает строку:
+var numObj = 12345.6789;
+numObj.toFixed(6);
+numObj.toFixed(); // '12345' - дробной части нет
+numObj.toFixed(1); // '12345.6'
+numObj.toFixed(6); // '12345.678900'
